ba0f3698fe123624374c33363a38b928ba30086f
Reuven Karasik <rubik@karasik.org>
2018-06-26 14:31:39 +0300

    server working

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-
 var app = express();
 app.use(bodyParser.json());
 
 var server = app.listen(process.env.PORT || 8080, function () {
 	var port = server.address().port;
 	console.log("App now running on port", port);
+});
+
+app.get("/", function (req, res) {
+	res.send('HEyyyy');
 });
 
 // CONTACTS API ROUTES BELOW
