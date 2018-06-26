f9f55af9778069eb58c03a66e6f083ebe963d52d
Reuven Karasik <rubik@karasik.org>
2018-06-26 15:06:11 +0300

    Added main view

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 
 var app = express();
 app.use(bodyParser.json());
+app.set('view engine', 'pug');
 
 var server = app.listen(process.env.PORT || 8080, function () {
 	var port = server.address().port;
@@ -10,7 +11,8 @@ var server = app.listen(process.env.PORT || 8080, function () {
 });
 
 app.get("/", function (req, res) {
-	res.send('HEyyyy');
+	// res.send('HEyyyy');
+	res.render('index');
 });
 
 // CONTACTS API ROUTES BELOW
